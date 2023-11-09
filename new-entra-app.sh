c2c918c3b426c362099c5fc2210b430005acedfc
mpflynnx <mpflynnx@gmail.com>
2023-11-09 15:18:59 +0000

    Commit to test action

--- new-entra-app.sh
+++ new-entra-app.sh
@@ -7,13 +7,36 @@ DEBUG=${DEBUG:-"NO"}
 
 
 cleanup() {
-  # Clean up by unmounting our loopmounts, deleting tempfiles:
+  # Clean up by deleting tempfiles:
   echo "--- Cleaning up ..."
   rm .env 2>${DBGOUT} || true
-#   rm credential.json 2>${DBGOUT} || true
+  rm credential.json 2>${DBGOUT} || true
 
 }
 trap 'echo "*** $0 FAILED at line $LINENO ***"; cleanup; exit 1' ERR INT TERM
+
+# AZ CLI check
+echo "Making sure you're signed in to Azure CLI..."
+az account show -o none
+
+if [ ! $? -eq 0 ]
+then
+    exit 1
+fi
+
+echo "Using the following Azure subscription. If this isn't correct, press Ctrl+C and select the correct subscription with \"az account set\""
+echo "${newline}"
+az account show -o table
+echo "${newline}"
+
+# GitHub CLI check
+echo "Making sure you're signed in to GitHub CLI..."
+gh auth status
+
+if [ ! $? -eq 0 ]
+then
+    exit 1
+fi
 
 # create new repo from static-website-azure-oidc template
 # login to gh cli locally
@@ -37,30 +60,48 @@ AZURE_SUBSCRIPTION_ID=$(az account show --query "id" --output tsv)
 # is this really required?
 #az account set -s $AZURE_SUBSCRIPTION_ID
 
-# Create the Microsoft Entra application.
-az ad app create --display-name ${APP_NAME}
-
-# New Service principal
+# check for existing app first
 CLIENT_ID=$(az ad app list --display-name ${APP_NAME} --query "[].appId" --output tsv)
 
-az ad sp create --id ${CLIENT_ID}
+if [ ! -z "$CLIENT_ID" ]
+then
+  echo "Application ${APP_NAME} already exists, exiting."
+  exit 1
+fi
 
-# ObjectId of the service principal
+# Create a Microsoft Entra application.
+echo "Creating new Microsoft Entra application...."
+az ad app create --display-name ${APP_NAME}
+CLIENT_ID=$(az ad app list --display-name ${APP_NAME} --query "[].appId" --output tsv)
+echo "Application Created."
+
+# Create new Service principal
+echo "Creating new Enterprise Application...."
+if [ ! -z "$CLIENT_ID" ]
+then
+  az ad sp create --id ${CLIENT_ID}
+fi
+
+# Get ObjectId of the service principal
 SP_OBJECT_ID=$(az ad sp list --display-name ${APP_NAME} --query "[].id" --output tsv)
 
 # Add role assignment
 # not working with --scope as of November 2023
-az role assignment create --role ${ROLE} --subscription ${AZURE_SUBSCRIPTION_ID} --assignee-object-id ${SP_OBJECT_ID} --assignee-principal-type ServicePrincipal
+if [ ! -z "SP_OBJECT_ID" ]
+then
+  az role assignment create \
+    --role ${ROLE} \
+    --subscription ${AZURE_SUBSCRIPTION_ID} \
+    --assignee-object-id ${SP_OBJECT_ID} \
+    --assignee-principal-type ServicePrincipal \
+    --output none
+fi
 
-# create a new federated identity credential
-# https://learn.microsoft.com/en-us/entra/workload-id/workload-identity-federation-create-trust?pivots=identity-wif-apps-methods-azcli
-# https://learn.microsoft.com/en-us/entra/workload-id/workload-identity-federation-create-trust?pivots=identity-wif-apps-methods-azp
-
-#needs object id of the app registration not service principal
+# Create a new federated identity credential
+# Needs object id of the app registration not service principal
 APP_OBJECT_ID=$(az ad app list --display-name ${APP_NAME} --query "[].id" --output tsv)
-#6295ef69-4a7a-4264-8ab2-0e6891f22ab6
 
-# create credential.json using heredoc
+# Create credential.json using Heredoc
 cat > "credential.json" << EOF
 {
     "name": "${APP_NAME}",
@@ -73,35 +114,13 @@ cat > "credential.json" << EOF
 }
 EOF
 
-az ad app federated-credential create --id ${APP_OBJECT_ID} --parameters credential.json
+echo "Adding federated credentials...."
+if [ ! -z "APP_OBJECT_ID" ]
+then
+  az ad app federated-credential create --id ${APP_OBJECT_ID} --parameters credential.json
+fi
 
-# alternative described here:
-# https://learn.microsoft.com/en-us/azure/storage/blobs/storage-blobs-static-site-github-actions?tabs=openid#generate-deployment-credentials
-#az rest --method POST --uri 'https://graph.microsoft.com/beta/applications/<APPLICATION-OBJECT-ID>/federatedIdentityCredentials' --body '{"name":"<CREDENTIAL-NAME>","issuer":"https://token.actions.githubusercontent.com","subject":"repo:organization/repository:ref:refs/heads/main","description":"Testing","audiences":["api://AzureADTokenExchange"]}'
-
-# search Default Directory | App registrations
-# static-website-github-action-oidc | Certificates & secrets
-
-# to run this file ./new-entra-app.sh
-
-#echo "Application Client ID: $CLIENT_ID"
-
-# set clientId as environmental variable or create .env file if multiple
-# needed
-
-# set up github cli
-
-# gh cli secret set
-#https://www.thegeekdiary.com/gh-secret-manage-github-secrets-from-the-command-line/
-
-#set secrets using gh cli from the $ENV_VALUE
-#gh secret set MYSECRET --body "$ENV_VALUE"
-
-#OR
-
-
-# use heredoc
-
+# Use Heredoc to create a .env file
 cat > ".env" << EOF
 AZURE_CLIENT_ID=${CLIENT_ID}
 AZURE_TENANT_ID=${AZURE_TENANT_ID}
@@ -110,8 +129,8 @@ EOF
 
 # Set multiple secrets imported from the ".env" file must make sure
 # .env is not part of VCS i.e .gitignore
+echo "Adding repository GitHub secrets to be referenced in your workflow...."
+
 gh secret set -f .env
 
-
-# cleanup
-
+cleanup
